2f04f006163398d1b0cf22532ded1db3061815f5
Lou-John <loujohn.genthon@gmail.com>
2023-12-14 10:52:27 +0100

    Initial commit

---
+++ src/main/java/com/example/test_spring_web/TestSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringWebApplication.class, args);
+	}
+
+}
